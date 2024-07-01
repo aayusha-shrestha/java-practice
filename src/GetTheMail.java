2f43dc0e8c0e6e8963e2d1585b0a95b38b574159
Aayusha <aayyushashrestha@gmail.com>
2024-07-01 23:07:39 +0545

    commented threads code

--- src/GetTheMail.java
+++ src/GetTheMail.java
@@ -1,20 +1,26 @@
+//
+	public class GetTheMail implements Runnable{
 
-public class GetTheMail implements Runnable{
-
-	private int startTime;
-	
-	public GetTheMail(int startTime) {
-		this.startTime = startTime;
-	}
-	
-	public void run() {
-		
-		
-		try {
-			Thread.sleep(startTime * 1000);
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			
 		}
-		catch(InterruptedException e) {}
-		
-		System.out.println("Checking Mail");
+//
+//	private int startTime;
+//	
+//	public GetTheMail(int startTime) {
+//		this.startTime = startTime;
+//	}
+//	
+//	public void run() {
+//		
+//		
+//		try {
+//			Thread.sleep(startTime * 1000);
+//		}
+//		catch(InterruptedException e) {}
+//		
+//		System.out.println("Checking Mail");
+//	}
 	}
-}
--- src/GetTime20.java
+++ src/GetTime20.java
@@ -1,35 +1,35 @@
-import java.util.*;
-import java.text.DateFormat;
-
-public class GetTime20 extends Thread{
-	
-	public void run() {
-		Date rightNow;
-		Locale currentLocale;
-		DateFormat timeFormatter;
-		DateFormat dateFormatter;
-		String timeOutput;
-		String dateOutput;
-		
-		for(int i = 1; i <= 20; i++) {
-			
-			rightNow = new Date();
-			currentLocale = Locale.forLanguageTag("en-US");
-			
-			timeFormatter = DateFormat.getTimeInstance(DateFormat.DEFAULT, currentLocale);
-			dateFormatter = DateFormat.getDateInstance(DateFormat.DEFAULT, currentLocale);
-			
-			timeOutput = timeFormatter.format(rightNow);
-			dateOutput = dateFormatter.format(rightNow);
-			
-			System.out.println(timeOutput);
-			System.out.println(dateOutput);
-			System.out.println();
-			
-			try {
-				Thread.sleep(2000); // sleeps every 2 sec for 20 iterations
-			}
-			catch(InterruptedException e) {}
-		}
+//import java.util.*;
+//import java.text.DateFormat;
+//
+	public class GetTime20 extends Thread{
+//	
+//	public void run() {
+//		Date rightNow;
+//		Locale currentLocale;
+//		DateFormat timeFormatter;
+//		DateFormat dateFormatter;
+//		String timeOutput;
+//		String dateOutput;
+//		
+//		for(int i = 1; i <= 20; i++) {
+//			
+//			rightNow = new Date();
+//			currentLocale = Locale.forLanguageTag("en-US");
+//			
+//			timeFormatter = DateFormat.getTimeInstance(DateFormat.DEFAULT, currentLocale);
+//			dateFormatter = DateFormat.getDateInstance(DateFormat.DEFAULT, currentLocale);
+//			
+//			timeOutput = timeFormatter.format(rightNow);
+//			dateOutput = dateFormatter.format(rightNow);
+//			
+//			System.out.println(timeOutput);
+//			System.out.println(dateOutput);
+//			System.out.println();
+//			
+//			try {
+//				Thread.sleep(2000); // sleeps every 2 sec for 20 iterations
+//			}
+//			catch(InterruptedException e) {}
+//		}
+//	}
 	}
-}
